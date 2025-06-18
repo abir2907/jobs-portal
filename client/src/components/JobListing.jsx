3f5ce77aa19318d0fa5dc894e7085345a93109aa
Abir Chodha <abirchodha1@gmail.com>
2025-06-18 19:39:47 +0530

    Add category & location filters and add styles

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,22 +1,22 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { assets } from "../assets/assets";
+import { assets, JobCategories, JobLocations } from "../assets/assets";
 
 const JobListing = () => {
   const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext);
 
   return (
-    <div>
+    <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
       {/* Sidebar */}
-      <div>
+      <div className="w-full lg:w-1/4 bg-white px-4">
         {/* Search Filter from hero component */}
         {isSearched &&
           (searchFilter.title !== "" || searchFilter.location !== "") && (
             <>
-              <h3>Current Search</h3>
-              <div>
+              <h3 className="font-medium text-lg mb-4">Current Search</h3>
+              <div className="mb-4 text-gray-600">
                 {searchFilter.title && (
-                  <span>
+                  <span className="inline-flex items-center gap-2.5 bg-blue-50 border border-blue-200 px-4 py-1.5 rounded">
                     {searchFilter.title}
                     <img
                       onClick={() =>
@@ -29,7 +29,7 @@ const JobListing = () => {
                   </span>
                 )}
                 {searchFilter.location && (
-                  <span>
+                  <span className="ml-2 inline-flex items-center gap-2.5 bg-red-50 border border-red-200 px-4 py-1.5 rounded">
                     {searchFilter.location}
                     <img
                       onClick={() =>
@@ -44,6 +44,32 @@ const JobListing = () => {
               </div>
             </>
           )}
+
+        {/* Category Filter */}
+        <div className="max-lg:hidden">
+          <h4 className="font-medium text-lg py-4">Search by Categories</h4>
+          <ul className="space-y-4 text-gray-600">
+            {JobCategories.map((category, index) => (
+              <li className="flex gap-3 items-center" key={index}>
+                <input className="scale-125" type="checkbox" name="" id="" />
+                {category}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Location Filter */}
+        <div className="max-lg:hidden">
+          <h4 className="font-medium text-lg py-4">Search by Location</h4>
+          <ul className="space-y-4 text-gray-600">
+            {JobLocations.map((location, index) => (
+              <li className="flex gap-3 items-center" key={index}>
+                <input className="scale-125" type="checkbox" name="" id="" />
+                {location}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
