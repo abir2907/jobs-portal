053a767c3cf2d21e64ece1a37c2f27226da86d83
Abir Chodha <abirchodha1@gmail.com>
2025-06-18 17:58:25 +0530

    Create company logos section in hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -42,6 +42,26 @@ const Hero = () => {
           </button>
         </div>
       </div>
+
+      <div className="border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex">
+        <div className="flex justify-center gap-10 lg:gap-16 flex-wrap">
+          <p className="font-medium">Trusted by</p>
+          <img
+            className="h-6"
+            src={assets.microsoft_logo}
+            alt="microsoft logo"
+          />
+          <img className="h-6" src={assets.walmart_logo} alt="walmart logo" />
+          <img
+            className="h-6"
+            src={assets.accenture_logo}
+            alt="accenture logo"
+          />
+          <img className="h-6" src={assets.samsung_logo} alt="samsung logo" />
+          <img className="h-6" src={assets.amazon_logo} alt="amazon logo" />
+          <img className="h-6" src={assets.adobe_logo} alt="adobe logo" />
+        </div>
+      </div>
     </div>
   );
 };
