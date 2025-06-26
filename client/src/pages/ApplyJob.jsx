0c4a279c3e088c8fdbadb25df65a2cd2bd373a0d
Abir Chodha <abirchodha1@gmail.com>
2025-06-26 18:46:58 +0530

    Fetch job from url id using useParams

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import { useParams } from "react-router-dom";
 
 const ApplyJob = () => {
+  const { id } = useParams();
+  const [JobData, setJobData] = useState(null);
+  const { jobs } = useContext(AppContext);
+
+  const fetchJob = async () => {
+    const data = jobs.filter((job) => job._id === id);
+    if (data.length !== 0) {
+      setJobData(data[0]);
+    }
+  };
+
+  useEffect(() => {
+    if (jobs.length > 0) {
+      fetchJob();
+    }
+  }, [id, jobs]);
+
   return <div>ApplyJob</div>;
 };
 
