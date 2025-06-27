a722e1eb8051ba97db15b93be039629e4358532a
Abir Chodha <abirchodha1@gmail.com>
2025-06-27 16:31:22 +0530

    Display other jobs from the same company

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { assets } from "../assets/assets";
 import Loading from "../components/Loading";
 import Navbar from "../components/Navbar";
+import JobCard from "../components/JobCard";
 import kConvert from "k-convert";
 import moment from "moment";
 
@@ -82,6 +83,21 @@ const ApplyJob = () => {
               <button className="bg-blue-600 p-2.5 px-10 text-white rounded cursor-pointer mt-10">
                 Apply Now
               </button>
+            </div>
+            {/* Right section, more jobs */}
+            <div className="w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5">
+              <h2>More jobs from {JobData.companyId.name}</h2>
+              {jobs
+                .filter(
+                  (job) =>
+                    job._id !== JobData._id &&
+                    job.companyId._id === JobData.companyId._id
+                )
+                .filter(() => true) // TODO: Filter already applied jobs
+                .slice(0, 4)
+                .map((job, index) => (
+                  <JobCard key={index} job={job} />
+                ))}
             </div>
           </div>
         </div>
