90a4c8c9e3a85d7b607f746c6ed8456f9ee5dfdc
Abir Chodha <abirchodha1@gmail.com>
2025-06-27 19:34:17 +0530

    Create header section of Applications page (resume select/edit)

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import { assets } from "../assets/assets";
 
 const Applications = () => {
-  return <div>Applications</div>;
+  const [isEdit, setIsEdit] = useState(false);
+  const [resume, setResume] = useState(null);
+
+  return (
+    <>
+      <Navbar />
+      <div className="container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10">
+        <h2 className="text-xl font-semibold">Your Resume</h2>
+        <div className="flex gap-2 mb-6 mt-3">
+          {isEdit ? (
+            <>
+              <label className="flex items-center" htmlFor="resumeUpload">
+                <p className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2 cursor-pointer">
+                  Select Resume
+                </p>
+                <input
+                  id="resumeUpload"
+                  onChange={(e) => setResume(e.target.files[0])}
+                  accept="application/pdf"
+                  type="file"
+                  hidden
+                />
+                <img
+                  className="cursor-pointer"
+                  src={assets.profile_upload_icon}
+                  alt="file upload icon"
+                />
+              </label>
+              <button
+                onClick={() => setIsEdit(false)}
+                className="bg-green-100 border border-green-400 rounded-lg px-4 py-2 cursor-pointer"
+              >
+                Save
+              </button>
+            </>
+          ) : (
+            <div className="flex gap-2">
+              <a
+                className="bg-blue-200 text-blue-600 px-4 py-2 rounded-lg"
+                href=""
+              >
+                Resume
+              </a>
+              <button
+                onClick={() => setIsEdit(true)}
+                className="text-gray-500 border border-gray-300 rounded-lg px-4 py-2 cursor-pointer"
+              >
+                Edit
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Applications;
