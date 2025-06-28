a0e758f71beac11a9ca7bdb67cb5f1390abeab89
Abir Chodha <abirchodha1@gmail.com>
2025-06-29 00:27:39 +0530

    Display next btn if data not submitted, else display sign up btn

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -11,59 +11,81 @@ const RecruiterLogin = () => {
 
   const [isTextDataSubmitted, setIsTextDataSubmitted] = useState(false);
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    if (state === "Sign Up" && !isTextDataSubmitted) {
+      setIsTextDataSubmitted(true);
+    }
+  };
+
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
-      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+      <form
+        onSubmit={onSubmitHandler}
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
         <h1 className="text-center text-2xl text-neutral-700 font-medium">
           Recruiter {state}
         </h1>
         <p className="text-sm">Welcome back! Please sign in to continue</p>
-        <>
-          {state !== "Login" && (
+        {state === "Sign Up" && isTextDataSubmitted ? (
+          <>{/* User can submit company logo */}</>
+        ) : (
+          <>
+            {state !== "Login" && (
+              <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+                <img src={assets.person_icon} alt="person icon" />
+                <input
+                  className="outline-none text-sm"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  type="text"
+                  placeholder="Company Name"
+                  required
+                />
+              </div>
+            )}
+
             <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
-              <img src={assets.person_icon} alt="person icon" />
+              <img src={assets.email_icon} alt="email icon" />
               <input
                 className="outline-none text-sm"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                type="text"
-                placeholder="Company Name"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type="email"
+                placeholder="Email Id"
                 required
               />
             </div>
-          )}
 
-          <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
-            <img src={assets.email_icon} alt="email icon" />
-            <input
-              className="outline-none text-sm"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              type="email"
-              placeholder="Email Id"
-              required
-            />
-          </div>
+            <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+              <img src={assets.lock_icon} alt="lock icon" />
+              <input
+                className="outline-none text-sm"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+          </>
+        )}
 
-          <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
-            <img src={assets.lock_icon} alt="lock icon" />
-            <input
-              className="outline-none text-sm"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              type="password"
-              placeholder="Password"
-              required
-            />
-          </div>
+        <p className="text-sm text-blue-600 my-4 cursor-pointer">
+          Forgot password?
+        </p>
 
-          <p className="text-sm text-blue-600 my-4 cursor-pointer">
-            Forgot password?
-          </p>
-        </>
-
-        <button className="bg-blue-600 w-full text-white py-2 rounded-full">
-          {state === "Login" ? "login" : "create account"}
+        <button
+          type="submit"
+          className="bg-blue-600 w-full text-white py-2 rounded-full"
+        >
+          {state === "Login"
+            ? "login"
+            : isTextDataSubmitted
+            ? "create account"
+            : "next"}
         </button>
 
         {state === "Login" ? (
