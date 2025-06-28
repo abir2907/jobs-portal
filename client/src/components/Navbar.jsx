ecb4b2221a49d7a7765d801fac9125081c65a7ed
Abir Chodha <abirchodha1@gmail.com>
2025-06-28 21:37:56 +0530

    Enable recruiter login on clicking RecruiterLogin

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { Link, useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
 
   const navigate = useNavigate();
+
+  const { setShowRecruiterLogin } = useContext(AppContext);
 
   return (
     <div className="shadow py-4">
@@ -29,7 +32,10 @@ const Navbar = () => {
           </div>
         ) : (
           <div className="flex gap-4 max-sm:text-xs">
-            <button className="text-gray-600 cursor-pointer">
+            <button
+              onClick={() => setShowRecruiterLogin(true)}
+              className="text-gray-600 cursor-pointer"
+            >
               Recruiter Login
             </button>
             <button
