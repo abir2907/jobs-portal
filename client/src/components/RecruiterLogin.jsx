2d4a963c1a7369ad83c4a39f228f320875727d4b
Abir Chodha <abirchodha1@gmail.com>
2025-06-28 21:52:14 +0530

    Create form for RecruiterLogin

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const RecruiterLogin = () => {
   const [state, setState] = useState("Login");
@@ -10,7 +11,50 @@ const RecruiterLogin = () => {
 
   const [isTextDataSubmitted, setIsTextDataSubmitted] = useState(false);
 
-  return <div>RecruiterLogin</div>;
+  return (
+    <div>
+      <form action="">
+        <h1>Recruiter {state}</h1>
+        <p>Welcome back! Please sign in to continue</p>
+        <>
+          <div>
+            <img src={assets.person_icon} alt="person icon" />
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              type="text"
+              placeholder="Company Name"
+              required
+            />
+          </div>
+
+          <div>
+            <img src={assets.email_icon} alt="email icon" />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              placeholder="Email Id"
+              required
+            />
+          </div>
+
+          <div>
+            <img src={assets.lock_icon} alt="lock icon" />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+        </>
+
+        <button>{state === "Login" ? "login" : "create account"}</button>
+      </form>
+    </div>
+  );
 };
 
 export default RecruiterLogin;
