2a3f60999a1ba2e2e95eda5ffa9ecd728f847291
Abir Chodha <abirchodha1@gmail.com>
2025-07-01 22:36:41 +0530

    Navigate to AddJob page on clicking add-job btn, Frontend Complete

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { manageJobsData } from "../assets/assets";
 import moment from "moment";
+import { useNavigate } from "react-router-dom";
 
 const ManageJobs = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="container p-4 max-w-5xl">
       <div className="overflow-x-auto">
@@ -48,7 +51,10 @@ const ManageJobs = () => {
       </div>
 
       <div className="mt-4 flex justify-end">
-        <button className="bg-black text-white py-2 px-4 rounded">
+        <button
+          onClick={() => navigate("/dashboard/add-job")}
+          className="bg-black text-white py-2 px-4 rounded cursor-pointer"
+        >
           Add new Job
         </button>
       </div>
