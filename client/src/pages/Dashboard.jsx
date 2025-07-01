0c8b7d4e82b3b3c894b16e3ae32970900bdacf0f
Abir Chodha <abirchodha1@gmail.com>
2025-07-01 17:51:17 +0530

    Style the dropdown

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -22,8 +22,8 @@ const Dashboard = () => {
                 alt="comapny icon"
               />
               <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
-                <ul>
-                  <li>Logout</li>
+                <ul className="list-none m-0 p-2 bg-white rounded-md border border-gray-200 text-sm">
+                  <li className="py-1 px-2 cursor-pointer pr-10">Logout</li>
                 </ul>
               </div>
             </div>
