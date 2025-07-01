1ea18d47cc8be4ae1d70fec62c936412ef415879
Abir Chodha <abirchodha1@gmail.com>
2025-07-01 19:29:27 +0530

    Create rich text editor using Quill for job description

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Quill from "quill";
 
 const AddJob = () => {
   const [title, setTitle] = useState("");
@@ -6,6 +7,18 @@ const AddJob = () => {
   const [category, setCategory] = useState("Programming");
   const [level, setLevel] = useState("Beginner level");
   const [salary, setSalary] = useState(0);
+
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+
+  useEffect(() => {
+    // Initiate Quill only once
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: "snow",
+      });
+    }
+  }, []);
 
   return (
     <form action="">
@@ -18,6 +31,11 @@ const AddJob = () => {
           value={title}
           required
         />
+      </div>
+
+      <div>
+        <p>Job Description</p>
+        <div ref={editorRef}></div>
       </div>
     </form>
   );
