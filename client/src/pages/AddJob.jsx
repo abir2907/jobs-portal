0949bf615c014e1399e91c788baec6ee2d785fc3
Abir Chodha <abirchodha1@gmail.com>
2025-07-01 19:06:17 +0530

    Start building AddJob page, create form with text input

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddJob = () => {
-  return <div>AddJob</div>;
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("Bangalore");
+  const [category, setCategory] = useState("Programming");
+  const [level, setLevel] = useState("Beginner level");
+  const [salary, setSalary] = useState(0);
+
+  return (
+    <form action="">
+      <div>
+        <p>Job Title</p>
+        <input
+          type="text"
+          placeholder="Type here"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
+        />
+      </div>
+    </form>
+  );
 };
 
 export default AddJob;
