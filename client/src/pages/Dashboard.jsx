0d5ae740c8a31f5fe3ae7d06854484ff74fb66c4
Abir Chodha <abirchodha1@gmail.com>
2025-07-01 17:34:40 +0530

    Design header to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-      <Outlet />
+    <div className="min-h-screen">
+      {/* Navbar for Recruiter Panel*/}
+      <div className="shadow py-4">
+        <div className="px-5 flex justify-between items-center">
+          <img
+            className="max-sm:w-32 cursor-pointer"
+            src={assets.logo}
+            alt="logo"
+          />
+          <div className="flex items-center gap-3">
+            <p className="max-sm:hidden">Welcome, Recruiter</p>
+            <div>
+              <img
+                className="w-8 border rounded-full"
+                src={assets.company_icon}
+                alt="comapny icon"
+              />
+              <div>
+                <ul>
+                  <li>Logout</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
