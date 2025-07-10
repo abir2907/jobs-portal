b6f204f934a9cb27afc2fcb1dafc1a66bfcbc625
Abir Chodha <abirchodha1@gmail.com>
2025-07-10 06:13:15 +0530

    Fix error: must return on first submit of sign-up page We do this to prevent the form from fully submitting on the first click of the "Sign Up" button. Instead, it triggers the UI to show the image upload step.

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -24,7 +24,7 @@ const RecruiterLogin = () => {
     e.preventDefault();
 
     if (state === "Sign Up" && !isTextDataSubmitted) {
-      setIsTextDataSubmitted(true);
+      return setIsTextDataSubmitted(true);
     }
 
     try {
