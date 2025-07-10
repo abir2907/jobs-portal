bd2f73e389c1d2f165ad7902601285fcf5fef3f3
Abir Chodha <abirchodha1@gmail.com>
2025-07-11 04:49:04 +0530

    Fetch and display company-specific job listings on ManageJobs page

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,10 +1,43 @@
 import React from "react";
-import { manageJobsData } from "../assets/assets";
 import moment from "moment";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const ManageJobs = () => {
   const navigate = useNavigate();
+
+  const [jobs, setJobs] = useState(false);
+
+  const { backendUrl, companyToken } = useContext(AppContext);
+
+  // Function to fetch company Job Applications data
+  const fetchCompanyJobs = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/company/list-jobs", {
+        headers: { token: companyToken },
+      });
+
+      if (data.success) {
+        setJobs(data.jobsData.reverse());
+        console.log(data.jobsData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (companyToken) {
+      fetchCompanyJobs();
+    }
+  }, [companyToken]);
 
   return (
     <div className="container p-4 max-w-5xl">
@@ -26,7 +59,7 @@ const ManageJobs = () => {
           </thead>
           <tbody>
             {/* Use dummy data for now */}
-            {manageJobsData.map((job, index) => (
+            {jobs.map((job, index) => (
               <tr key={index} className="text-gray-700">
                 <td className="py-2 px-4 border-b max-sm:hidden">
                   {index + 1}
