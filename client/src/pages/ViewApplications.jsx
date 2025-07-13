0fce887d7ec832e91cdfca988d0a54c01febbbf1
Abir Chodha <abirchodha1@gmail.com>
2025-07-14 05:19:46 +0530

    Fix small css bug

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -60,7 +60,7 @@ const ViewApplications = () => {
                     <td className="py-2 px-4 border-b text-center">
                       {index + 1}
                     </td>
-                    <td className="py-2 px-4 border-b text-center flex">
+                    <td className="py-2 px-4 border-b text-center flex items-center">
                       <img
                         className="w-10 h-10 rounded-full mr-3 max-sm:hidden"
                         src={applicant.userId.image}
